9109777463145d5cbd245659392a794056f2bfd5
nico <nd@nocoffeetech.de>
2022-04-12 12:45:55 +0200

    MyTypes.h documentation done

--- src/MyTypes/MyTypes.h
+++ src/MyTypes/MyTypes.h
@@ -30,13 +30,36 @@ enum class InterpolationType {
     Checkerboard, Black_White
 };
 
-
+/**
+ * @brief Writes \p vector_to_write to \p root as attribute with the name \p name
+ * @param vector_to_write Vector to be written
+ * @param root Group to write to
+ * @param name Name of the target Attribute
+ */
 void WriteVectorX(VectorX &vector_to_write, HighFive::Group &root, std::string name);
 
+/**
+ * @brief Reads \p vector_to_read_into from \p root from the attribute with the name \p name
+ * @param vector_to_read_into Vector to read into
+ * @param root Group to read from
+ * @param name Name of the target Attribute
+ */
 void ReadVectorX(VectorX &vector_to_read_into, HighFive::Group &root, std::string name);
 
+/**
+ * @brief Writes \p matrix_to_write to \p root as attribute with the name \p name
+ * @param matrix_to_write Matrix to be written
+ * @param root Group to write to
+ * @param name Name of the target Attribute
+ */
 void WriteMatrixX(MatrixX &matrix_to_write, HighFive::Group &root, std::string name);
 
+/**
+ * @brief Reads \p matrix_to_read_into from \p root from the attribute with the name \p name
+ * @param matrix_to_read_into Matrix to read into
+ * @param root Group to read from
+ * @param name Name of the target Attribute
+ */
 void ReadMatrixX(MatrixX &matrix_to_read_into, HighFive::Group &root, std::string name);
 
 #endif //BACHELOR_THESIS_MYTYPES_H
