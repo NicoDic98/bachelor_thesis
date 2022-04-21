df4bf5c4b4d14694ea80e0e72ba74e74379d1728
nico <nd@nocoffeetech.de>
2022-04-21 12:53:12 +0200

    started plotting

--- plot/plot.py
+++ plot/plot.py
@@ -1,14 +1,28 @@
 import h5py
 import os
-import matplotlib
+import matplotlib.pyplot as plt
 
-
+betas = []
+inverse_betas=[]
+magnetizations = []
+magnetizations_squared = []
 for file in os.listdir():
     if file.startswith("out"):
         print(file)
         f = h5py.File(file, 'r')
 
-        level0 = f.get(list(f.keys())[0])
-        meas = level0.get(list(level0.keys())[0])
-        magn = meas.get(list(meas.keys())[0])
-        print(dict(magn.attrs.items()))
+        level0_group = f.get("level0")
+
+        measurements_group = level0_group.get("measurements")
+
+        betas.append(level0_group.attrs["beta"])
+        inverse_betas=1./betas[-1]
+        magnetization_group = measurements_group.get("magnetization")
+        magnetizations.append(magnetization_group.attrs["mean"])
+
+        magnetization_squared_group = measurements_group.get("magnetization_squared")
+        magnetizations_squared.append(magnetization_squared_group.attrs["mean"])
+
+
+plt.plot(betas, magnetizations)
+
