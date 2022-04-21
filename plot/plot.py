c7c14a9a49bfee0ab3f7169890c389cc9b310162
nico <nd@nocoffeetech.de>
2022-04-21 13:13:03 +0200

    started plotting

--- plot/plot.py
+++ plot/plot.py
@@ -1,11 +1,16 @@
 import h5py
 import os
+
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 
 betas = []
-inverse_betas=[]
+inverse_betas = []
 magnetizations = []
+magnetizations_errors = []
 magnetizations_squared = []
+magnetizations_squared_errors = []
 for file in os.listdir():
     if file.startswith("out"):
         print(file)
@@ -16,13 +21,25 @@ for file in os.listdir():
         measurements_group = level0_group.get("measurements")
 
         betas.append(level0_group.attrs["beta"])
-        inverse_betas=1./betas[-1]
+        inverse_betas.append(1. / betas[-1])
         magnetization_group = measurements_group.get("magnetization")
-        magnetizations.append(magnetization_group.attrs["mean"])
+        magnetizations.append(magnetization_group.attrs["bootstrap_mean"])
+        magnetizations_errors.append(np.sqrt(magnetization_group.attrs["bootstrap_variance"]))
 
         magnetization_squared_group = measurements_group.get("magnetization_squared")
         magnetizations_squared.append(magnetization_squared_group.attrs["mean"])
 
 
-plt.plot(betas, magnetizations)
+def magnetization_exact(beta):
+    if beta < 0.440686793509772:
+        return 0
+    else:
+        return (1. - 1 / np.sinh(2 * beta) ** 4) ** (1 / 8)
 
+print(magnetizations_errors)
+fig, ax = plt.subplots()
+ax.errorbar(inverse_betas, magnetizations, magnetizations_errors, fmt='o')
+beta_lin = np.linspace(0.25, 2, 100)
+m_exact = [magnetization_exact(temp) for temp in beta_lin]
+plt.plot(1. / beta_lin, m_exact)
+plt.savefig("test")
